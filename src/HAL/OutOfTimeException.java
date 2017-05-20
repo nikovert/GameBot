819c3d118a2ffd35aa2309f78e351cf28c9c4c51
Felix.Crazzolara <felix.crazzolara@gmail.com>
2017-05-20 11:05:35 +0200

    Bugs fixed

---
+++ src/HAL/OutOfTimeException.java
@@ -0,0 +1,12 @@
+package HAL;
+
+public class OutOfTimeException extends RuntimeException {
+
+  int movecounter;
+  
+  public OutOfTimeException(){}
+  
+  public OutOfTimeException(int movecounter){
+    this.movecounter = movecounter;
+  }
+}
